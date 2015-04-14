7e040589b812b478dab011b3ce3917e7b232d7b6
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-14 11:24:41 +0100

    Set up ApplicationController
    Include Slimmer headers and shared templates and throw a 404 error if
    GdsApi returns a not found.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Slimmer::Headers
+  include Slimmer::SharedTemplates
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from GdsApi::HTTPNotFound, with: :error_not_found
+
+private
+  def error_not_found
+    render status: :not_found, text: "404 error not found"
+  end
 end
