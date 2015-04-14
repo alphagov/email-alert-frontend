dd02ff9a7522730f50be77e0fdfe0636f3a335a1
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-14 11:35:08 +0100

    Include frontend env configuration
    Following the example in other Frontend Apps, this commit adds some
    config options to the production env and application.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['GOVUK_ASSET_HOST']
+  config.slimmer.use_cache = true
+  config.slimmer.asset_host = Plek.current.find('static')
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
