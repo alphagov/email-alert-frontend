8f9ce2eb06798cb7bbb01bb02bb494091e58ca24
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-16 13:15:11 +0100

    Add services registry
    Initially, we'll need the Content Store and Email Alert API. This commit
    adds the service registry along with these 2 services.

---
+++ config/initializers/services.rb
@@ -0,0 +1,19 @@
+require 'gds_api/email_alert_api'
+require 'gds_api/content_store'
+
+module EmailAlertFrontend
+  def self.register_service(name, service)
+    @services ||= {}
+
+    @services[name] = service
+  end
+
+  def self.services(name)
+    @services[name] or raise ServiceNotRegisteredException.new(name)
+  end
+
+  class ServiceNotRegisteredException < Exception; end
+end
+
+EmailAlertFrontend.register_service(:email_alert_api, GdsApi::EmailAlertApi.new(Plek.new.find('email-alert-api')))
+EmailAlertFrontend.register_service(:content_store, GdsApi::ContentStore.new(Plek.new.find('content-store')))
