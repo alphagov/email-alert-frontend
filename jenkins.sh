346f1e28c48f45ed6354f6bcdccbbcf9e8779e86
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-17 14:30:06 +0100

    Add jenkins file
    For both master builds and branch builds.

---
+++ jenkins.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -x
+export DISPLAY=:99
+export GOVUK_APP_DOMAIN=test.gov.uk
+export GOVUK_ASSET_ROOT=http://static.test.gov.uk
+export REPO_NAME="alphagov/email-alert-frontend"
+env
+
+function github_status {
+  REPO_NAME="$1"
+  STATUS="$2"
+  MESSAGE="$3"
+  gh-status "$REPO_NAME" "$GIT_COMMIT" "$STATUS" -d "Build #${BUILD_NUMBER} ${MESSAGE}" -u "$BUILD_URL" >/dev/null
+}
+
+function error_handler {
+  trap - ERR # disable error trap to avoid recursion
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+  else
+    echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
+  fi
+  github_status "$REPO_NAME" failure "failed on Jenkins"
+  exit "${code}"
+}
+
+trap "error_handler ${LINENO}" ERR
+github_status "$REPO_NAME" pending "is running on Jenkins"
+
+# This tests that the current branch can be cleanly merged into master
+git merge --no-commit origin/master || git merge --abort
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
+
+# This is run to ensure that assets precompile
+bundle exec rake assets:precompile
+
+# Clone govuk-content-schemas depedency for tests
+rm -rf tmp/govuk-content-schemas
+git clone git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
+
+RAILS_ENV=test GOVUK_CONTENT_SCHEMAS_PATH=tmp/govuk-content-schemas bundle exec rake
+
+export EXIT_STATUS=$?
+
+if [ "$EXIT_STATUS" == "0" ]; then
+  github_status "$REPO_NAME" success "succeeded on Jenkins"
+else
+  github_status "$REPO_NAME" failure "failed on Jenkins"
+fi
+
+exit $EXIT_STATUS
