37ecea4d173db3acbbb792ad9e512f7cb81e59ae
Mo Baig <mo@mobaig.com>
2017-03-24 01:28:33 +0000

    Use mock email-alert-api in email alert feature, also test redirects
    - Use the verifying instance double of email-alert-api in the existing
      email alert feature.
    - Setup a route specific to the test environment to test redirecting to
      govdelivery subscription url.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-
   get '/*base_path' => 'email_alert_signups#new', as: :email_alert_signup, constraints: { base_path: %r|.*/email-signup|}
   post '/*base_path' => 'email_alert_signups#create', as: :email_alert_signups, constraints: { base_path: %r|.*/email-signup|}
+
+  if Rails.env.test?
+    get '/govdelivery-redirect', to: proc { [200, {}, ['']] }
+  end
 end
