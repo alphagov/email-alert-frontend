4449345099b1431bf2547dc65f72e975ea4ed06f
Chris Patuzzo <chris@patuzzo.co.uk>
2017-12-07 17:48:09 +0000

    Handle error cases when creating a subscription
    https://trello.com/c/amAa5HkV/444-handle-error-cases-for-capturing-a-users-email-address
    
    - show an error if the user tries to submit
    without an address
    - show an error if the user tries to submit with
    an invalid address
    - preserve the address input text if the form
    fails to submit
    - add test coverage for other error cases, e.g.
    when no subscribable exists for the topic_id
    
    Additionally, we probably don’t want to expose the
    internal database id for a subscribable in the
    form, so this looks up the subscribable again from
    the topic_id parameter in #create.

--- spec/features/subscribe_spec.rb
+++ spec/features/subscribe_spec.rb
@@ -4,20 +4,21 @@ require 'gds_api/test_helpers/email_alert_api'
 RSpec.describe "subscribing", type: :feature do
   include GdsApi::TestHelpers::EmailAlertApi
 
+  let(:topic_id) { "GOVUK_123" }
+  let(:subscribable_id) { 10 }
+  let(:address) { "test@test.com" }
+
   context "successfully" do
     before do
       email_alert_api_has_subscribable(
-        reference: "test135",
+        reference: topic_id,
         returned_attributes: {
-          id: 10,
+          id: subscribable_id,
           title: "Test Subscriber List"
         }
       )
 
-      subscribable_id = "10"
-      address = "test@test.com"
       returned_subscription_id = 50
-
       email_alert_api_creates_a_subscription(
         subscribable_id,
         address,
@@ -26,8 +27,8 @@ RSpec.describe "subscribing", type: :feature do
     end
 
     it "subscribes and renders the success page" do
-      visit "/email/subscriptions/new?topic_id=test135"
-      fill_in :address, with: "test@test.com"
+      visit "/email/subscriptions/new?topic_id=#{topic_id}"
+      fill_in :address, with: address
       expect(page).to have_content("Test Subscriber List")
       click_button "Subscribe"
       expect(page).to have_content("Subscription successfully created")
