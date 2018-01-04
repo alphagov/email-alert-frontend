7e4c9875cb64754c9e5e90a575de778bd9d6d434
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-04 11:33:32 +0000

    Remove a bottom margin CSS rule that is handled by components

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,5 +17,5 @@ Rails.application.config.assets.digest = true
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(
-  unsubscriptions.css
+  application-without-elements.css
 )
