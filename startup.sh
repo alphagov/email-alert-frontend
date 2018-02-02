7f063e05f6f2b7e561400c77ed32be8cb6fe8ca8
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-02-02 14:33:40 +0000

    Add GOVUK_APP_DOMAIN_EXTERNAL env vars

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ bundle install
 
 if [[ $1 == "--live" ]] ; then
   GOVUK_APP_DOMAIN=www.gov.uk \
+  GOVUK_APP_DOMAIN_EXTERNAL=www.gov.uk \
   GOVUK_WEBSITE_ROOT=https://www.gov.uk \
   PLEK_SERVICE_CONTENT_STORE_URI=https://www.gov.uk/api \
   PLEK_SERVICE_STATIC_URI=assets.publishing.service.gov.uk \
