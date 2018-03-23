8d2c0ace6e8f669329885fb00d9fad5637ad6fc5
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-03-23 13:54:07 +0000

    Tweak styling of unsubscribe pages
    This commit tweaks the styling of the unsubscribe pages to look more like the subscription management pages, and to offer back and cancel links when the journey starts from the subscription management flow.

--- app/controllers/unsubscriptions_controller.rb
+++ app/controllers/unsubscriptions_controller.rb
@@ -1,6 +1,6 @@
 class UnsubscriptionsController < ApplicationController
   before_action :set_cache_control_header
-  before_action :set_title, :set_uuid
+  before_action :set_title, :set_uuid, :set_back_url, :set_from
 
   def confirm; end
 
@@ -23,6 +23,23 @@ private
 
   def set_uuid
     @uuid = params[:uuid].presence
+  end
+
+  def set_back_url
+    @back_url = list_subscriptions_path
+  end
+
+  def set_from
+    @from = from
+    @from_subscription_management = from_subscription_management?
+  end
+
+  def from
+    params.permit(:from).fetch(:from, "")
+  end
+
+  def from_subscription_management?
+    from == "subscription-management"
   end
 
   def api
