da2e2a0022f9620fd48c96a1c8b95aad7bafd815
Tijmen Brommet <tijmen@gmail.com>
2018-06-08 08:24:58 +0100

    Use govspeak component from gem
    The Govspeak component has moved from Static to the gem. This switches
    this app to use the gem-based component.
    
    To make the transition easier I've added a `govspeak` helper that takes
    a block. This cleans up the views a lot and makes it easier to read the
    HTML. It also makes sure that we're using Rails' HTML escaping
    mechanism by default, which is needed because the component will in the
    future no longer call `.html_safe` on the input
    (https://github.com/alphagov/govuk_publishing_components/pull/356).
    This fixes at least one (unexploitable) [XSS vulnerability in this
    app][1] (`@address` is add to the page unescaped).
    
    [1]:
    https://github.com/alphagov/email-alert-frontend/blob/73dc8d4db0174b215f
    c61c42fa8ad992f2d8f6d2/app/views/authentication/request_sign_in_token.ht
    ml.erb#L17
    
    https://trello.com/c/uwcWXXNp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def govspeak
+    render "govuk_publishing_components/components/govspeak", rich_govspeak: true do
+      yield
+    end
+  end
+
   def title(text, params = {})
     render 'govuk_publishing_components/components/title', { title: text }.merge(params)
   end
