8f9cf14c4ab425b5a3544e0d702cdfe7e8ab7bcf
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2018-12-28 18:49:02 +0000

    Remove unused JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,1 @@
-// from govuk_frontend_toolkit and not delivered by static as part of
-// header-footer-only on deployed environments
-//=require govuk/show-hide-content.js
-//
-// from govuk_publishing_components
-//=require govuk_publishing_components/all_components
-;(function () {
-  var showHideContent = new GOVUK.ShowHideContent()
-  showHideContent.init()
-})()
+// =require govuk_publishing_components/all_components
