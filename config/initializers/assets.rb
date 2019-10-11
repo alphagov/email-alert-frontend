88f3889c43aa24f9b033b57cd0a26b1ffeb80b24
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-11 14:59:08 +0100

    Update govuk_publishing_components and add missing assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,11 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.precompile += %w(
+  application.js
+  application.css
+  print.css
+)
 
 Rails.application.config.assets.prefix = "/email-alert-frontend"
 
