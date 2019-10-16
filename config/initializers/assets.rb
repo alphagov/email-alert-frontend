7dfb627b73969ece28f7ddff5f61aad5a49e5558
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-16 15:28:31 +0100

    Add manifest.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,9 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
-
-# Add additional assets to the asset load path.
 Rails.application.config.assets.precompile += %w(
   application.js
   application.css
@@ -12,12 +6,6 @@ Rails.application.config.assets.precompile += %w(
 
 Rails.application.config.assets.prefix = "/email-alert-frontend"
 
-Rails.application.config.assets.digest = true
+Rails.application.config.assets.version = "1.0"
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(
-  print.css
-)
+Rails.application.config.assets.digest = true
