9783890d0881c4c27f7c10f3dc0df69c55c437cb
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-12-04 17:57:39 +0000

    Update subscribe feature and fix initial views
    This modifies the 'Subscribe' feature to now end by checking an 'opt in'
    email has been sent. Since the new view will use localisations for its
    text, it was a good opportunity to refactor the text out of the existing
    views for entering the frequency and the address.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.hosts << "email-alert-frontend.dev.gov.uk"
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
--- spec/features/subscribe_spec.rb
+++ spec/features/subscribe_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Subscribe" do
     when_i_visit_the_email_signup_page
     and_i_choose_a_frequency
     and_i_enter_my_email_address
-    then_i_should_be_subscribed
+    then_i_should_receive_an_opt_in_email
   end
 
   def given_there_is_content_i_can_subscribe_to
@@ -27,7 +27,7 @@ RSpec.feature "Subscribe" do
   end
 
   def and_i_choose_a_frequency
-    expect(page).to have_content("How often do you want to get updates?")
+    expect(page).to have_content(I18n.t!("subscriptions.new_frequency.question"))
     @frequency = "weekly"
     choose "frequency", option: @frequency, visible: false
     click_on "Next"
@@ -35,25 +35,23 @@ RSpec.feature "Subscribe" do
 
   def and_i_enter_my_email_address
     expect(back_link_href).to include(new_subscription_path(topic_id: @topic_id))
-    expect(page).to have_content("What’s your email address?")
+    expect(page).to have_content(I18n.t!("subscriptions.new_address.question"))
 
     address = "test@test.com"
     fill_in :address, with: address
 
-    @request = email_alert_api_creates_a_subscription(
-      @subscriber_list_id,
+    @request = stub_email_alert_api_sends_subscription_verification_email(
       address,
       @frequency,
-      "returned_subscriber_id",
+      @topic_id,
     )
 
     click_on "Subscribe"
   end
 
-  def then_i_should_be_subscribed
+  def then_i_should_receive_an_opt_in_email
     expect(@request).to have_been_requested
-    expect(page).to have_content("You’ve subscribed successfully")
-    expect(page).to have_content("Test Subscriber List")
+    expect(page).to have_content(I18n.t!("subscriptions.check_email.title"))
   end
 
   def back_link_href
