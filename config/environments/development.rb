7b9cf819caacdac89d45281326ce454f1c12c108
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-12-06 11:18:17 +0000

    Relax authorized host domains in development
    This changes the development configuration to allow requests from
    any domain. While this will include *.dev.gov.uk, this reduces the
    coupling to that specific domain without any extra effort, thus
    supporting more use cases like Docker training.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,8 @@ Rails.application.configure do
 
   config.asset_host = ENV["GOVUK_ASSET_ROOT"] if ENV["GOVUK_ASSET_ROOT"].present?
 
-  config.hosts << "email-alert-frontend.dev.gov.uk"
+  # Allow requests for all domains e.g. <app>.dev.gov.uk
+  config.hosts.clear
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
