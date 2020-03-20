d687702d215b77d8454d5ad4e63255639879991e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-20 11:41:27 +0000

    Skip 'options' page in email subscription journey if there are no options to choose from

--- spec/features/topic_signup_back_spec.rb
+++ spec/features/topic_signup_back_spec.rb
@@ -1,10 +1,9 @@
 RSpec.feature "Topic signup back" do
   include GovukContentSchemaExamples
 
-  scenario "live taxon" do
-    document = GovukSchemas::RandomExample.for_schema(frontend_schema: "taxon") do |doc|
-      doc.merge("phase" => "live")
-    end
+  scenario "live taxon with children" do
+    document = GovukSchemas::Example.find("taxon", example_name: "taxon_with_child_taxons")
+    document.merge!("phase" => "live")
 
     content_store_has_item(document["base_path"], document)
     visit "/email-signup?topic=#{document['base_path']}"
@@ -12,12 +11,39 @@ RSpec.feature "Topic signup back" do
     expect(page).to have_link "Back", href: document["base_path"]
   end
 
-  scenario "alpha taxon" do
-    document = GovukSchemas::RandomExample.for_schema(frontend_schema: "taxon") do |doc|
-      doc.merge("phase" => "alpha")
-    end
+  scenario "alpha taxon with children" do
+    document = GovukSchemas::Example.find("taxon", example_name: "taxon_with_child_taxons")
+    document.merge!("phase" => "alpha")
 
     content_store_has_item(document["base_path"], document)
+    visit "/email-signup?topic=#{document['base_path']}"
+
+    expect(page).to_not have_link "Back", href: document["base_path"]
+  end
+
+  scenario "live taxon without children" do
+    document = GovukSchemas::Example.find("taxon", example_name: "taxon")
+    document.merge!("phase" => "live")
+
+    content_store_has_item(document["base_path"], document)
+    content_store_has_item(
+      document["links"]["parent_taxons"].first["base_path"],
+      document["links"]["parent_taxons"].first,
+    )
+    visit "/email-signup?topic=#{document['base_path']}"
+
+    expect(page).to have_link "Back", href: "javascript:history.back()"
+  end
+
+  scenario "alpha taxon without children" do
+    document = GovukSchemas::Example.find("taxon", example_name: "taxon")
+    document.merge!("phase" => "alpha")
+
+    content_store_has_item(document["base_path"], document)
+    content_store_has_item(
+      document["links"]["parent_taxons"].first["base_path"],
+      document["links"]["parent_taxons"].first,
+    )
     visit "/email-signup?topic=#{document['base_path']}"
 
     expect(page).to_not have_link "Back", href: document["base_path"]
