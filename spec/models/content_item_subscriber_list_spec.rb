8e7ef1e29098381ab6c79089f125839e00e7f912
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-28 15:40:48 +0100

    DRY-up tests for ContentItemSubscriberList
    Previously we were repeating the same expectations over and over,
    even though only one output changes between the contexts. This
    removes the unnecessary repetition, noting that it's still necessary
    to call 'subscription_management_url' in order to trigger the API
    call under test. We will remove this oddity in a future commit.

--- spec/models/content_item_subscriber_list_spec.rb
+++ spec/models/content_item_subscriber_list_spec.rb
@@ -4,131 +4,90 @@ RSpec.describe ContentItemSubscriberList do
       instance_double(EmailAlertFrontend.services(:email_alert_api).class)
     end
 
+    let(:content_item) do
+      { "title" => "Foo", "content_id" => "foo-id" }
+    end
+
     before do
       allow(EmailAlertFrontend)
         .to receive(:services)
         .with(:email_alert_api)
         .and_return(mock_email_alert_api)
+
       allow(mock_email_alert_api)
         .to receive(:find_or_create_subscriber_list)
         .and_return("subscriber_list" => { "subscription_url" => "/something" })
     end
 
-    context "given a taxon" do
-      it "asks email-alert-api to find or create a subscriber list" do
-        taxon = { "document_type" => "taxon",
-                  "title" => "Foo",
-                  "content_id" => "foo-id",
-                  "base_path" => "/taxy/taxon" }
+    it "creates a subscriber list for taxons" do
+      content_item.merge!("document_type" => "taxon")
+      signup = described_class.new(content_item)
 
-        signup = described_class.new(taxon)
+      expect(signup.has_content_item?).to be
+      expect(signup.subscription_management_url).to eq "/something"
 
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "Foo", "links" => { "taxon_tree" => %w[foo-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "taxon_tree" => %w[foo-id] })
     end
 
-    context "when no content item is present" do
-      it "does nothing" do
-        signup = described_class.new(nil)
-
-        expect(signup.has_content_item?).to_not be
-      end
+    it "does nothing when no content item is present" do
+      signup = described_class.new(nil)
+      expect(signup.has_content_item?).to_not be
     end
 
-    context "given an organisation" do
-      organisation = { "document_type" => "organisation", "title" => "Org", "content_id" => "org-id" }
+    it "creates a subscriber list for an organisation" do
+      content_item.merge!("document_type" => "organisation")
+      described_class.new(content_item).subscription_management_url
 
-      it "asks email-alert-api to find or create a subscriber list" do
-        signup = described_class.new(organisation)
-
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "Org", "links" => { "organisations" => %w[org-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "organisations" => %w[foo-id] })
     end
 
-    context "given a person" do
-      person = { "document_type" => "person", "title" => "Peter", "content_id" => "person-id" }
+    it "creates a subscriber list for a person" do
+      content_item.merge!("document_type" => "person")
+      described_class.new(content_item).subscription_management_url
 
-      it "asks email-alert-api to find or create a subscriber list" do
-        signup = described_class.new(person)
-
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "Peter", "links" => { "people" => %w[person-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "people" => %w[foo-id] })
     end
 
-    context "given a ministerial role" do
-      ministerial_role = { "document_type" => "ministerial_role",
-                           "title" => "pm",
-                           "content_id" => "pm-id" }
+    it "creates a subscriber list for a ministerial role" do
+      content_item.merge!("document_type" => "ministerial_role")
+      described_class.new(content_item).subscription_management_url
 
-      it "asks email-alert-api to find or create a subscriber list" do
-        signup = described_class.new(ministerial_role)
-
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "pm", "links" => { "roles" => %w[pm-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "roles" => %w[foo-id] })
     end
 
-    context "given a topical event" do
-      topical_event = { "document_type" => "topical_event",
-                        "title" => "Summit 2019",
-                        "content_id" => "summit-id" }
+    it "creates a subscriber list for a topical event" do
+      content_item.merge!("document_type" => "topical_event")
+      described_class.new(content_item).subscription_management_url
 
-      it "asks email-alert-api to find or create a subscriber list" do
-        signup = described_class.new(topical_event)
-
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "Summit 2019", "links" => { "topical_events" => %w[summit-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "topical_events" => %w[foo-id] })
     end
 
-    context "given a service manual topic" do
-      topical_event = { "document_type" => "service_manual_topic",
-                        "title" => "Foo",
-                        "content_id" => "foo-id" }
+    it "creates a subscriber list for a service manual topic" do
+      content_item.merge!("document_type" => "service_manual_topic")
+      described_class.new(content_item).subscription_management_url
 
-      it "asks email-alert-api to find or create a subscriber list" do
-        signup = described_class.new(topical_event)
-
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "Foo", "links" => { "service_manual_topics" => %w[foo-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "service_manual_topics" => %w[foo-id] })
     end
 
-    context "given the service standard" do
-      topical_event = { "document_type" => "service_manual_service_standard",
-                        "title" => "Foo",
-                        "content_id" => "foo-id" }
+    it "creates a subscriber list for the service standard" do
+      content_item.merge!("document_type" => "service_manual_service_standard")
+      described_class.new(content_item).subscription_management_url
 
-      it "asks email-alert-api to find or create a subscriber list" do
-        signup = described_class.new(topical_event)
-
-        expect(signup.has_content_item?).to be
-        expect(signup.subscription_management_url).to eq "/something"
-        expect(mock_email_alert_api)
-          .to have_received(:find_or_create_subscriber_list)
-          .with("title" => "Foo", "links" => { "parent" => %w[foo-id] })
-      end
+      expect(mock_email_alert_api)
+        .to have_received(:find_or_create_subscriber_list)
+        .with("title" => "Foo", "links" => { "parent" => %w[foo-id] })
     end
   end
 end
