a743198242ffd4f67da1da64c9b95dc8ab61fc9e
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2020-07-08 16:43:54 +0100

    Load dependencies in test only

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/error-summary
