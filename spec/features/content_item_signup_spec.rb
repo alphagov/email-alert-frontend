c7bc08c061d4101c56c96ca039df0f1d6fce931c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-11-24 12:05:51 +0000

    Update content item signup new with new design
    This also reorders the radio buttons as per the design, combining
    them into a single component. We will look at not having a default
    selection in future work.

--- spec/features/content_item_signup_spec.rb
+++ spec/features/content_item_signup_spec.rb
@@ -62,13 +62,13 @@ RSpec.feature "Content item signup" do
 
     visit new_content_item_signup_path(topic: @taxon[:base_path])
     expect(page).to have_content(@taxon[:title])
-    expect(page).to have_checked_field("all-0")
+    expect(page).to have_checked_field("topic-2")
     expect(page).to have_content(@taxon.dig(:links, :child_taxons).first[:title])
   end
 
   def and_i_refine_my_selection
     choose @taxon[:title]
-    click_button "Select"
+    click_button "Continue"
   end
 
   def and_i_click_to_signup_to_alerts
