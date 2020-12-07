7e7af948c311de5159853d97152d10035f948de3
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-12-07 12:31:42 +0000

    Rewrite unsubscribe spec to test two scenarios
    Previously we only tested the unauthenticated journey. This rewrites
    the feature to cover the two scenarios where someone may unsubscribe:
    
    - With an unauthenticated link in a digest email.
    
    - Via the manage subscriptions page (after authenticating).
    
    This is a precursor to changing the journey so that the user must be
    authenticated when unsubscribing.
    
    Note that previously we were incorrectly passing the ID of the list
    as the ID of the subscription for one of the test stubs.

--- spec/features/unsubscribe_spec.rb
+++ spec/features/unsubscribe_spec.rb
@@ -1,30 +1,73 @@
-RSpec.feature "Unsubscribe after receiving confirmation link" do
+RSpec.feature "Unsubscribe" do
   include GdsApi::TestHelpers::EmailAlertApi
+  include TokenHelper
 
-  scenario do
+  scenario "when using a one-click link" do
     given_i_have_a_subscription
     when_i_visit_the_unsubscribe_page
-    and_i_click_on_unsubscribe
+    and_i_confirm_to_unsubscribe
     then_i_see_that_i_am_unsubscribed
   end
 
+  scenario "when managing subscriptions" do
+    given_i_have_a_subscription
+    and_i_have_a_secret_sign_in_token
+    when_i_visit_the_management_page
+    and_i_click_on_unsubscribe
+    and_i_confirm_to_unsubscribe
+    then_i_see_the_management_page
+  end
+
   def given_i_have_a_subscription
-    @subscriber_list_id = SecureRandom.uuid
+    @subscription_id = SecureRandom.uuid
     @title = "A thing to subscribe to"
+    @subscriber_id = SecureRandom.uuid
+
     stub_email_alert_api_has_subscription(
-      @subscriber_list_id,
+      @subscription_id,
       "immediately",
       title: @title,
+      subscriber_id: @subscriber_id,
     )
   end
 
   def when_i_visit_the_unsubscribe_page
-    visit confirm_unsubscribe_path(@subscriber_list_id)
+    visit confirm_unsubscribe_path(@subscription_id)
+  end
+
+  def and_i_have_a_secret_sign_in_token
+    @address = "test@test.com"
+
+    @token = encrypt_and_sign_token(data: {
+      "address" => @address,
+      "subscriber_id" => @subscriber_id,
+    })
+  end
+
+  def when_i_visit_the_management_page
+    stub_email_alert_api_has_subscriber_subscriptions(
+      @subscriber_id,
+      @address,
+      nil,
+      subscriptions: [
+        {
+          "id" => @subscription_id,
+          "created_at" => Time.zone.now,
+          "subscriber_list" => {},
+        },
+      ],
+    )
+
+    visit process_sign_in_token_path(token: @token)
   end
 
   def and_i_click_on_unsubscribe
+    click_on "Unsubscribe"
+  end
+
+  def and_i_confirm_to_unsubscribe
     @unsubscribe_request = stub_email_alert_api_unsubscribes_a_subscription(
-      @subscriber_list_id,
+      @subscription_id,
     )
     click_on "Unsubscribe"
   end
@@ -33,6 +76,13 @@ RSpec.feature "Unsubscribe after receiving confirmation link" do
     expect(@unsubscribe_request).to have_been_requested
     expect(page).to have_content(
       I18n.t!("unsubscriptions.confirmation.with_title", title: @title),
+    )
+  end
+
+  def then_i_see_the_management_page
+    expect(current_path).to eq list_subscriptions_path
+    expect(page).to have_content(
+      "You have been unsubscribed from ‘#{@title}’",
     )
   end
 end
