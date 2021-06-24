958f6e21611cba2f541591d88b2dfb11f57f145c
Michael Walker <mike@barrucadu.co.uk>
2021-06-24 10:59:27 +0100

    Add a controller method to authenticate a user by GOV.UK Account
    We added a new endpoint to email-alert-api to authenticate a user by
    GOV.UK Account, returning the subscriber details if the email address
    is verified and matches a subscriber.  This commit makes the
    corresponding change on the email-alert-frontend side, exposing that
    feature to users.
    
    This is not complete, we still need to solve some design issues, like:
    
    - What do we show in the error cases?
    
    - Do we prompt a user to explicitly "link" the accounts, rather than
    just sign them in?
    
    - Do we forbid users from changing their notifications email address
    if it's linked to an account?  Would it be confusing if the addresses
    get out of sync?
    
    So for now it's hidden behind a feature flag.
    
    See also https://github.com/alphagov/email-alert-api/pull/1638

--- app/controllers/subscriber_authentication_controller.rb
+++ app/controllers/subscriber_authentication_controller.rb
@@ -1,4 +1,5 @@
 class SubscriberAuthenticationController < ApplicationController
+  include GovukPersonalisation::AccountConcern
   include SessionsHelper
 
   def sign_in
@@ -30,6 +31,29 @@ class SubscriberAuthenticationController < ApplicationController
 
     authenticate_subscriber(token.data[:subscriber_id])
     redirect_to list_subscriptions_path
+  end
+
+  def process_govuk_account
+    head :not_found and return unless ENV["FEATURE_FLAG_GOVUK_ACCOUNT"] == "enabled"
+
+    api_response = GdsApi.email_alert_api.authenticate_subscriber_by_govuk_account(govuk_account_session: account_session_header)
+    set_account_session_header(api_response["govuk_account_session"])
+    authenticate_subscriber(api_response.dig("subscriber", "id"))
+    redirect_to list_subscriptions_path
+  rescue GdsApi::HTTPUnauthorized
+    deauthenticate_subscriber
+    logout!
+    uri = GdsApi.account_api.get_sign_in_url(redirect_path: process_govuk_account_path)["auth_uri"]
+    uri += "&_ga=#{params[:_ga]}" if params[:_ga]
+    redirect_to uri
+  rescue GdsApi::HTTPForbidden => e
+    deauthenticate_subscriber
+    set_account_session_header(JSON.parse(e.http_body)["govuk_account_session"])
+    render plain: "This GOV.UK account does not have a verified email address."
+  rescue GdsApi::HTTPNotFound => e
+    deauthenticate_subscriber
+    set_account_session_header(JSON.parse(e.http_body)["govuk_account_session"])
+    render plain: "This GOV.UK account does not have a notifications account."
   end
 
 private
