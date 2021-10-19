a7cc30c6cf4289214144192f7a653842ddc574cc
Michael Walker <mike@barrucadu.co.uk>
2021-10-19 14:52:57 +0100

    Add a confirmation page for creating an account subscription
    This page handles signing up to notifications connected to the
    account, which means it also handles the "linking" of an account-api
    `OidcUser` to an email-alert-api `Subscriber`.
    
    If a user has subscriptions which pre-date their account, we'll list
    them so that the user knows what is changing.  The linking process
    also triggers an email in this case (email-alert-api does this).
    
    This page is intentionally very generic: it's not just for single-page
    notifications, or content item subscriptions, it's for topics in
    general.  This is so we can use this page across different sorts of
    email journeys.

---
+++ app/controllers/account_subscriptions_controller.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+class AccountSubscriptionsController < ApplicationController
+  include AccountHelper
+  include FrequenciesHelper
+  include GovukPersonalisation::ControllerConcern
+
+  DEFAULT_FREQUENCY = "daily"
+  SUCCESS_FLASH = "email-subscription-success"
+
+  before_action do
+    head :not_found unless govuk_account_auth_enabled?
+  end
+
+  before_action do
+    @topic_id = subscription_parameters.fetch(:topic_id)
+    @subscriber_list = GdsApi.email_alert_api.get_subscriber_list(slug: @topic_id).to_h.fetch("subscriber_list")
+    @frequency = subscription_parameters.fetch(:frequency, DEFAULT_FREQUENCY)
+
+    unless valid_frequencies.include?(@frequency)
+      redirect_with_analytics confirm_account_subscription_path(
+        topic_id: @topic_id,
+        return_to_url: @return_to_url,
+      )
+    end
+  end
+
+  rescue_from GdsApi::HTTPUnauthorized, with: :reauthenticate_user
+  rescue_from GdsApi::HTTPForbidden, with: :reauthenticate_user
+
+  def confirm
+    response = GdsApi.email_alert_api.authenticate_subscriber_by_govuk_account(
+      govuk_account_session: account_session_header,
+    )
+    set_account_session_header(response["govuk_account_session"])
+    subscriber = response.to_h.fetch("subscriber")
+
+    @address = subscriber.fetch("address")
+
+    @unlinked_subscriptions =
+      if subscriber["govuk_account_id"]
+        []
+      else
+        GdsApi.email_alert_api.get_subscriptions(id: subscriber.fetch("id")).to_h.fetch("subscriptions", [])
+      end
+  end
+
+  def create
+    response = GdsApi.email_alert_api.link_subscriber_to_govuk_account(
+      govuk_account_session: account_session_header,
+    )
+    set_account_session_header(response["govuk_account_session"])
+    subscriber = response.to_h.fetch("subscriber")
+
+    GdsApi.email_alert_api.subscribe(
+      subscriber_list_id: @subscriber_list.fetch("id"),
+      address: subscriber.fetch("address"),
+      frequency: @frequency,
+    )
+
+    account_flash_add SUCCESS_FLASH
+
+    if subscription_parameters[:return_to_url].blank? || @subscriber_list["url"].blank?
+      redirect_to process_govuk_account_path
+    else
+      redirect_to @subscriber_list["url"]
+    end
+  end
+
+private
+
+  def subscription_parameters
+    params.permit(:topic_id, :frequency, :return_to_url)
+  end
+
+  def reauthenticate_user
+    logout!
+
+    redirect_with_analytics GdsApi.account_api.get_sign_in_url(
+      redirect_path: confirm_account_subscription_path(
+        topic_id: @topic_id,
+        frequency: @frequency,
+        return_to_url: @return_to_url,
+      ),
+    )["auth_uri"]
+  end
+end
