3bf8d74216e00c23c33c6f52cd442d880daf54d7
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2022-05-16 16:44:50 +0100

    Clean up component assets
    - removing some assets that are not needed, because this application doesn't include those components
    - also removing some that are already included in static, because they're duplicated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,6 @@
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/error-summary
-//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/radio
 
 //= require_tree ./modules
