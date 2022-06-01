110c4925a3d8f6801d265ee05bce2fef675a8a96
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-06-01 11:44:30 +0100

    Allow AuthToken to accept SHA256 or SHA1 hashes
    Makes SHA256 tokens the default again (as per Rails 7 defaults)
    Adds a rotate to the decrypter that allows it to accept SHA1 tokens as well as SHA256 tokens.
    This will allow it to accept the current SHA1 tokens from email-alert-api, and the SHA256 tokens from when email-alert-api is updated properly.
    
    https://trello.com/c/Iz4TyAzx/1321-fixes-for-email-alert-api-email-alert-frontend-update-incident
    
    Note - due to the way email-alert-api is set up, not currently possible to test this end to end. Has been tested by generating a token in the email-alert-api integration console:
    
    token = AuthTokenGeneratorService.call(hello: 'world')
    
    ...then checking it decrypts on the email-alert-frontend integration console:
    
    irb(main):007:0> token = AuthToken.new(data)
    => #<AuthToken:0x00007f23de69fb78 @token="1xZjrFbz4korhQywWP9H2nycQBNdySnzq...
    irb(main):008:0> token.valid?
    => true
    irb(main):009:0> token.data
    => {:hello=>"world"}

--- spec/support/token_helper.rb
+++ spec/support/token_helper.rb
@@ -1,8 +1,8 @@
 module TokenHelper
-  def encrypt_and_sign_token(data: {}, expiry: 5.minutes)
+  def encrypt_and_sign_token(data: {}, expiry: 5.minutes, hash_digest_class: OpenSSL::Digest::SHA256)
     len = ActiveSupport::MessageEncryptor.key_len(AuthToken::CIPHER)
     secret = Rails.application.secrets.email_alert_auth_token
-    key = ActiveSupport::KeyGenerator.new(secret).generate_key("", len)
+    key = ActiveSupport::KeyGenerator.new(secret, hash_digest_class: hash_digest_class).generate_key("", len)
     crypt = ActiveSupport::MessageEncryptor.new(key, **AuthToken::OPTIONS)
     crypt.encrypt_and_sign(data, expires_in: expiry)
   end
