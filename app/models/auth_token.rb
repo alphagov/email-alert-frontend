77c7e32e3442919bb29b3c95ee2976e3bbab2481
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-06-06 16:43:06 +0100

    Remove ability to accept SHA1 tokens.
    By the time this is merged, email-alert-api should have been generating 
    only  generating SHA256 tokens for a week, so no SHA1 tokens are left in 
    the wild.

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -22,8 +22,6 @@ private
     len = ActiveSupport::MessageEncryptor.key_len(CIPHER)
     key = ActiveSupport::KeyGenerator.new(secret).generate_key("", len)
     crypt = ActiveSupport::MessageEncryptor.new(key, **OPTIONS)
-    key_sha1 = ActiveSupport::KeyGenerator.new(secret, hash_digest_class: OpenSSL::Digest::SHA1).generate_key("", len)
-    crypt.rotate(key_sha1)
     decrypted_data = crypt.decrypt_and_verify(token)
     decrypted_data&.symbolize_keys
   rescue ActiveSupport::MessageEncryptor::InvalidMessage
