de50008365fde84afa594fa2e80527d4fc17baed
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-12-12 16:18:31 +0000

    Update class names for the namespaced service

--- app/services/subscriber_list_params/generate_links_based_list_params_service.rb
+++ app/services/subscriber_list_params/generate_links_based_list_params_service.rb
@@ -1,51 +1,53 @@
-class GenerateSubscriberListParamsService < ApplicationService
-  def initialize(content_item)
-    super()
-    @content_item = content_item
-  end
-
-  def call
-    {
-      "title" => content_item["title"],
-      "links" => link_hash,
-      "url" => content_item["base_path"],
-    }
-  end
-
-private
-
-  attr_reader :content_item
-
-  class UnsupportedContentItemError < StandardError; end
-
-  def link_hash
-    case content_item_type
-    when "taxon"
-      single_link(key: "taxon_tree")
-    when "topic"
-      single_link(key: "topics")
-    when "organisation"
-      single_link(key: "organisations")
-    when "person"
-      single_link(key: "people")
-    when "ministerial_role"
-      single_link(key: "roles")
-    when "topical_event"
-      single_link(key: "topical_events")
-    when "service_manual_topic"
-      single_link(key: "service_manual_topics")
-    when "service_manual_service_standard"
-      single_link(key: "parent")
-    else
-      raise UnsupportedContentItemError
+module SubscriberListParams
+  class GenerateLinksBasedListParamsService < ApplicationService
+    def initialize(content_item)
+      super()
+      @content_item = content_item
     end
-  end
 
-  def single_link(key:)
-    { key => [content_item["content_id"]] }
-  end
+    def call
+      {
+        "title" => content_item["title"],
+        "links" => link_hash,
+        "url" => content_item["base_path"],
+      }
+    end
 
-  def content_item_type
-    content_item["document_type"]
+  private
+
+    attr_reader :content_item
+
+    class UnsupportedContentItemError < StandardError; end
+
+    def link_hash
+      case content_item_type
+      when "taxon"
+        single_link(key: "taxon_tree")
+      when "topic"
+        single_link(key: "topics")
+      when "organisation"
+        single_link(key: "organisations")
+      when "person"
+        single_link(key: "people")
+      when "ministerial_role"
+        single_link(key: "roles")
+      when "topical_event"
+        single_link(key: "topical_events")
+      when "service_manual_topic"
+        single_link(key: "service_manual_topics")
+      when "service_manual_service_standard"
+        single_link(key: "parent")
+      else
+        raise UnsupportedContentItemError
+      end
+    end
+
+    def single_link(key:)
+      { key => [content_item["content_id"]] }
+    end
+
+    def content_item_type
+      content_item["document_type"]
+    end
   end
 end
