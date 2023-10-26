e0311c2909df3a096db7f2fa4b779837dd753db8
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-10-26 13:19:19 +0100

    Update all references to deprecated Rails.applications.secrets to Rails.application.credentials

--- spec/support/token_helper.rb
+++ spec/support/token_helper.rb
@@ -1,7 +1,7 @@
 module TokenHelper
   def encrypt_and_sign_token(data: {}, expiry: 5.minutes, hash_digest_class: OpenSSL::Digest::SHA256)
     len = ActiveSupport::MessageEncryptor.key_len(AuthToken::CIPHER)
-    secret = Rails.application.secrets.email_alert_auth_token
+    secret = Rails.application.credentials.email_alert_auth_token
     key = ActiveSupport::KeyGenerator.new(secret, hash_digest_class:).generate_key("", len)
     crypt = ActiveSupport::MessageEncryptor.new(key, **AuthToken::OPTIONS)
     crypt.encrypt_and_sign(data, expires_in: expiry)
