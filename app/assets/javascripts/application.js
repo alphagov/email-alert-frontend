65234807d895b31445d087b8b9646c1cd83d78f7
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-08-20 19:22:24 +0100

    WIP - improve match to original

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/layout-super-navigation-header
