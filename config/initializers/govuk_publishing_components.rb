6c1482bb65d01b14c68631f60d4977aae9ab6d7d
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-12 16:16:51 +0000

    Use component directly
    - for the moment, include all components/JS

--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -1,4 +1,5 @@
 GovukPublishingComponents.configure do |c|
   c.component_guide_title = "Email Alert Frontend Component Guide"
   c.application_stylesheet = "application"
+  c.exclude_css_from_static = false
 end
