598599f46b83e2ba610d31551b480cd1992ed6d2
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-02-10 15:36:09 +0000

    Add basic layout with direct component calls
    - Update layout to use layout_for_public component directly
    - Update JS to include JS support for layout components
    - Update SCSS file to build component support CSS
      We won't get this from static any more
    - Add exclude_css_from_static = false to configuration
    - Layout does not include account-specific code, and has
      a section explicitly adding gem_component CSS for the components
      added by layout_for_public (since these can't be included
      automatically). Once added, we call render_component_stylesheets
      as part of the content_for :head, which is yielded to in
      the layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,6 @@
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/layout-super-navigation-header
