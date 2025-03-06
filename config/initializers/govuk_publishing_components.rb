d462740922506d24d1868f705300cb6dfb1713cc
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-06 14:01:46 +0000

    Add basic elements bundle
    - although we could bring in all these elements as individual
      stylesheets, the compression benefit we get by bundling all
      these styles (which are used on all pages in the app)
      together is considerable. In tests the bundled styles led
      to an overall style size when compressed of 20 Kb (compared
      to 34 Kb with all individual styles, or 22 Kb with the
      current static system). There may be further refinements
      we can make, but for the moment this is reasonable.
    - We bundle the basic component stylesheets in the application.css
    - We also need to add these to the custom_css_exclude_list
      so that the app doesn't duplicate them by bringing them
      in as individual files too.
    - Also include the JS files needed for these components
      (previously supplied by static's JS file)

--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -1,4 +1,18 @@
 GovukPublishingComponents.configure do |c|
   c.component_guide_title = "Email Alert Frontend Component Guide"
   c.application_stylesheet = "application"
+  c.custom_css_exclude_list = %w[
+    button
+    cookie-banner
+    feedback
+    heading
+    input
+    label
+    layout-footer
+    layout-for-public
+    layout-super-navigation-header
+    search
+    search-with-autocomplete
+    skip-link
+  ]
 end
