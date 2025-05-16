d58fefd9342adf7c262cc65f79bfc5b616ed529e
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2025-05-16 16:41:47 +0100

    Use the service navigation component

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,7 @@
 //= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/layout-super-navigation-header
 //= require govuk_publishing_components/components/search-with-autocomplete
+//= require govuk_publishing_components/components/service-navigation
 //= require govuk_publishing_components/components/skip-link
 
 //= require govuk_web_banners/dependencies
